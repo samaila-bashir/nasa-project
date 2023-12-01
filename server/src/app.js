d55e74a4ca4ddbb40a811d83231c0c8ed5eb0618
Samaila Chatto Bashir <chattobashir@gmail.com>
2023-12-01 19:56:39 +0100

    created a fullstack NASA project

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const planetsRouter = require('./routes/planets/planets.router');
+
+const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:3001',
+  })
+);
+app.use(express.json());
+app.use(planetsRouter);
+
+module.exports = app;
